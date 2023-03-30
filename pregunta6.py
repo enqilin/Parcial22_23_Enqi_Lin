79c76cf490709bbdb907fef16e9170e5e5cad605
nadous <114181724+enqilin@users.noreply.github.com>
2023-03-30 08:12:00 +0000

    determinante iterativa

--- pregunta6.py
+++ pregunta6.py
@@ -1,18 +1,37 @@
-class nodo(object):
-    def __init__(self):
-        self.info = None
-        self.sif = None
-class datoPolinomio(object):
-    def __init__(self,valor, termino):
-        self.valor=valor
-        self.termino = termino
+matriz= []
+def mat(n):
+    for i in range(n):
+        matriz.append([])
+        for j in range(n):
+            matriz[i].append(0)
+    return matriz
+def llenar_matriz(n):
+    matriz = mat(n)
+    for i in range(n):
+        for j in range(n):
+            matriz[i][j]= int(input("El valor de ", str(x), "][",str(y),"]= "))
+def ordenar(n):
+    ma=llenar_matriz(n)
+    for i in range(n):
+        for j in range(n):
+            if ma[0][0]==0:
+                ma[0][:] = ma[1][:]
+                ma[1][:]= ma[0][:]
+            else:
+                pass
+def guass(n):
+    for z in range(n-1):
+        for x in range(n-z):
+            if (matriz[z][z]!=0):
+                p = matriz[z+x][z]/ matriz[z][z]
+                for y in range(n):
+                    matriz[x+z][y] = matriz[x+z][y]*p
 
-class Polinomio(object):
-    def __init__(self, termino_mayor = None,tamanio = -1):
-        self.termino_mayor=termino_mayor
-        self.tamanio = tamanio
-    def agregar_termino(polinomio,termino , valor):
+def det(n):
+    detr=1
+    for i in range(n):
+        detr = matriz[i][i]*detr
+    print("\n el determinante de la matriz es = ", detr)
 
-        aux=nodo()
-        dato = datoPolinomio(valor,termino)
-        aux.info=dato
+def detr(n)
+    
